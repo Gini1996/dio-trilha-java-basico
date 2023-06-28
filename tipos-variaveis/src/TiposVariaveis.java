15e5911d75c189908ededef936c040d83e28bc4d
LEONARDO GINI <leonardo.gini.96@hotmail.com>
2023-06-28 15:32:47 -0300

    Estudo de Tipos de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception 
+    {
+       String meuNome = "Leonardo Ferreira";
+       double salarioMinimo = 2500;
+       int idade = 27;
+       float peso = 105.5F;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       //Casting de int para short pois int possui tamanho base maior, gerando assim erro caso não seja aplicada a conversão
+       short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+       numero = 10;
+
+       //Final torna a variavel constante
+       //Variaveis final utilizam Caixa Alta
+       final double VALOR_DE_PI = 3.14;
+
+       System.out.println("Olá, eu sou ".concat(meuNome).concat("\nTenho ") + idade + " anos.".concat(" \nPeso atualmente ") + peso + " quilos.");
+       System.out.println("O custo de vida medio em meu país é de: R$" + salarioMinimo);
+       System.out.println("Imprimindo um numero convertido: " + numeroCurto2);
+       System.out.println("Variavel comum: " + numero);
+       System.out.println("PI é imutavel assim como sua variavel: " + VALOR_DE_PI);
     }
 }
