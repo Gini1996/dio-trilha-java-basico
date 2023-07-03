1863dfa34f8d402ae6314ef2b7d5a3fa2f967e70
LEONARDO GINI <leonardo.gini.96@hotmail.com>
2023-07-03 17:00:27 -0300

    Documentacao com Java

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,18 @@
+/**
+ * @author Leonardo Ferreira
+ * @version 1.0
+ * @since 03/07/2023
+ */
+public class Calculadora 
+{
+    /**
+     * Este método retorna a soma de dois valores inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar( int numeroUm, int numeroDois )
+    {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,9 @@
+/**
+ * Isso aqui e feito para documentar essa parada aqui
+ */
+public class Comentarios
+{
+  //Comentario de linha unica
+  /*Opa,
+    Comentario de várias linhas */   
+}
