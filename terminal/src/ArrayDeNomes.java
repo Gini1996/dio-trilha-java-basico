da818f55067e8f0ecf4271040ceaf215fb7a9d38
LEONARDO GINI <leonardo.gini.96@hotmail.com>
2023-07-05 16:25:26 -0300

    Comentarios

--- terminal/src/ArrayDeNomes.java
+++ terminal/src/ArrayDeNomes.java
@@ -4,8 +4,8 @@ public class ArrayDeNomes
 {
     public static void main(String[] args)
     {
-    
         Scanner sc = new Scanner(System.in);
+		//O numero do new String é o numero total, de 1 em diante e nao posições do Array
       	String[] nomes = new String[10];
 		
 		for(int i=0 ; i<10 ; i++)
